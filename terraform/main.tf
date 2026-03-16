0d68c6a0b225edbd745b83934d8250052831839a
mvcardim <mvcardim@gmail.com>
2026-03-16 13:42:41 +0000

    fix: corrige sintaxe do bloco attribute no DynamoDB

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,18 @@
 # Bucket S3
 resource "aws_s3_bucket" "app" {
   bucket = "app-bucket-cicd"
-  tags   = { Environment = "localstack", ManagedBy = "terraform" }
+  tags = {
+    Environment = "localstack"
+    ManagedBy   = "terraform"
+  }
 }
 
 # Fila SQS
 resource "aws_sqs_queue" "app" {
   name = "app-queue-cicd"
-  tags = { Environment = "localstack" }
+  tags = {
+    Environment = "localstack"
+  }
 }
 
 # Tabela DynamoDB
@@ -15,6 +20,13 @@ resource "aws_dynamodb_table" "app" {
   name         = "app-table-cicd"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
-  attribute { name = "id" type = "S" }
-  tags = { Environment = "localstack" }
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Environment = "localstack"
+  }
 }
